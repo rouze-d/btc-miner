a5ad4001fd15f6dbb8f6e9b90c5a188e804f05bd
Rouze <50823809+rouze-d@users.noreply.github.com>
2021-04-25 22:55:16 +0800

    Update btcminer.sh

--- btcminer.sh
+++ btcminer.sh
@@ -1,4 +1,119 @@
 #!/bin/bash
 
-God="BlessYou";krF=FoL;Gnr=Vl;QIy=Is;UOt=FC;kVE=Sk;KlM=gM;Xwe=TF;oTX=dD;Yox=Qa;wQx=mb;Kqb=Zy;xWU=twB;gTp=auw;hwZ=Ejp;tHg=iQZ;ojo=shb;jhw=FjF;IDL=klV;wOA=yFT;PZL=XfL;DRJ=DiE;bmJ=FCa;XGN=dOT;QCK=dkY;SOb=WJE;RJx=uAv;czJ=dts;VGB=oCy;TyO=xGj;yHU=bpq;Nfb=Wct;doQ=Ibf;XCL=rax;ZNH=Hhf;Eoh=yCU;ghd=hut;ooz=VKY;qMf=PrY;Ecf=Lke;BbJ=jie;QOI=lOT;oi=QkU;yM=iDO;rT=twI;wa=BEk;Lf=SGl;Sx=Kri;yL=RiF;Xf=iZT;iv=fpC;fW=nki;qX=FrL;ba=OUY;czP=Hdm;ecN=WKt;QMW=BnB;Nsq=jdC;nof=OZt;bhx=qNV;vVN=xaU;smp=Wta;ceY=GDh;AIB=juO;miA=Cso;zgz=Hka;Kea=YMq;hUp=SWY;zMP=ozk;WGK=JEt;pkF=jjF;FCq=pFs;ZrQ=VcQ;ZjS=do;BTP=Su;sly=xM;aZl=vF;iJn=bo;QCl=HB;SCO=xo;fUq=Jo;Epq=zc;wxq=Ev;ziJ=Gx;Kzo=JF;PDO=pJ;qhq=yp;OYk=rj;OGy=Cc;hSK=xR;lnD=um;UXi=pl;xBF=xP;qOD=iX;RVy=nl;mqe=JK;rQt=CA;qJD=FR;sPp=XZ;moh=DC;Fsj=GWh;NTx=Jnx;dwG=ncr;Ugd=mFV;ZTt=WjI;mFG=lms;YGK=bPI;UKs=ibv;hka=JFE;zpQ=Gxt;VlF=Jxk;AEJ=cuk;SUZ=YiV;Wfq=NhZ;xLp=ray;QmE=mqM;fkp=tHl;Sek=XvT;qbj=EKu;RfA=xSe;fPQ=Oll;cif=jtg;GYE=yei;Prw=RTD;sMB=pEL;sxA=APb;aRs=HgZ;SXm=VSI;lZA=UKc;YMw=Lct;bwO=XQI;OxQ=COX;bPh=KqP;GVr=hRx;eoA=eID;AZK=fFj;PGC=XPy;ieV=gly;AUO=yit;GjW=jaZ;tBB=HSO;vdV=efl;HiW=VHx;JaQ=RCo;cGr=fsk;eib=jty;TPK=rlb;dhM=oDt;tCL=muK;gAg=xQq;xWl=eMt;AtB=mbQ;UzS=lee;xjU=CPZ;qLS=iIg;dQw=NHN;Dum=qAc;aei=Whw;afz=OcR;NWZ=Txs;gzI=HsZ;YmH=IIm;kZc=Ppd;qnZ=vjw;sQw=BmF;PAB=rGe;gah=aHu;XfD=uJJ;LmJ=PUK;jCz=XpN;gTg=hRF;cpS=fhx;wFI=fih;pHM=aTH;GIm=Jtb;XDy=XGY;zMs=kPd;VtP=pbP;ALj=acU;HEZ=Tre;EPr=vOf;eSC=AIk;TXH=szR;nNa=bGD;lQt=Qod;Qvm=HGI;tkS=IOY;qmZ=tDf;xxx=xOS;pFg=XCd;aVR=xGh;ooR=AHX;JVj=laY;TvM=kEx;SYU=hol;ddj=khS;aPP=oiI;Edo="
-";DSi=WYX;LHA=Gfx;VgU=gJZ;Rxh=xYy;bXG=gvs;eLoP="e";u18="6";Yt="l";Yxgs="";qwt="Fu";ieC=HO;XcH=Rv;Ugs=LC;vD=xp;fy=oW;Zl=LhH;oP=Jm;sDA=kpY;GHn=TB;wzk=Da;oiO=Ei;NaM=hIf;fC=vjz;kT=rz;sV=bZz;CL=ARL;Wc=dA;gZG=tGI;vpk=cM;Aao=tgy;sU=ld;LZ=Pr;BE=wCX;iN=fQ;RCe=Dm;Rc=Fl;Njo=OMq;gtb=SKa;Wf=jMB;Dm=wHr;Nx=Wn;ANT=jAK;oFy=Bnc;IjZ=Xj;DQa=oK;ewZ=Vm;QPf=lz;Cwg=aS;KEM=jS;fkE=hM;nLw=ym;FWv=BC;aDC=Lw;tW=Rgn;kw=Hgf;tQ=eSh;Fi=iGb;Or=snM;ZG=xqx;xC=WJr;Bf=haC;pT=THr;By=jbu;tm=Hcy;eT=nTC;UT=KCa;dk=Pxy;vG=TvX;HX=MMB;Ow=ZoT;UJ=LvE;rx=oMF;EB=VZx;OC=IfU;dv=lTx;Nk=TNq;Ej=bKu;dl=ZZi;yzL=bJ;EiG=LJ;iqg=gK;fbd=vg;OPl=Ei;tnI=YS;yBV=DA;yxh=Ks;Nic=Ih;wMq=tD;iDs=AQ;Nhh=zy;JWe=Ry;gC=AnI;qWN=uq;hIc=OAL;UOm=Nq;Xj=ax;GW=utn;Fmc=CaI;zeK=eT;hI=sl;Ae=RK;XV=doy;Dpq=Eg;Mmr=MZg;CXd=Bao;RND=WUC;CGm=SRK;DJh=LZw;ym=Ah;xzv=IX;FYi=YUX;v6T=" ";RzE="pIDImFGdlNHI0VHc0hCJ94URFJ1RKkyMgYWY0V2cgQXdwRHKk0zVPxETFllCKg2chJ2LulmYvEyI hRXZzBCd1BHdoQSPHd0RKkCNgYWY0V2cgQXdwRHKk0TRVxkQKkSMgYWY0V2cgQXdwRHKk0DRFJlC gQXdwRHKk0DRM9kQKkCMgI3ZzBCd1BHdoQSPE5UQUNlCpcDImFGdlNHI0VHc0hCJ94UWDpQK1AiZ iPLliDCIzSp4gMJliDCITSp4BSp4zSp4gAykUKegUK+jUKOIgICIl1CIvh2YlpgCiMiCKkCZs9mY iPJliDCITSp4BSp4zSp4gAysUKOIzSp4gACIgACITSp4BSp4PSp4gAykUK+kUK+jUKOIgMJliHIl gAymUKegUK+gUKOIgsJli/IlifJliDCIbSp4zSp4DSp4gACIgMIliDCIKMLliDysUKOIgMJliHIl iPIliDCITSp4BSp4XSp4gAyqUKegUK+gUKOIgsKliHIliPIliDCIgACIgMIliDygUKOIgAygUKOI BSp4bSp4gACIHSp4gACIgAyhUKOIgAyhUKOIgAymUK+lUK+hUKOIgsJliHIlifJliDCIKsKliHIl TSp4PSp4PSp4gAiC7Sp4gcIliDCIbSp4BSp4BSp4gAyhUKOIbSp4gAyuUKOIHSp4gACIgACIbSp4 iDCIDSp4gAygUK+gUK+gUKOIgoAITSp4BSp4zSp4gAykUKegUK+sUKOIgMJliPJli/IliDCIvBCI HSp4gAyhUKOIbSp4gAiCyVmbp1EIul2bDRXaCBymUK+sUK+gUKOIgASgUK+oUKOIgMIliPIliPIl zFGSvRHc5J3QgcCVQllUDN1JggGdpdHIbSp4XSp4HSp4gAymUKegUK+uUKOIgsJlifJlifIliDCI gACIgACIgACIgACIgQkTBR1UkICIl1CIvh2YlpwNuADIw1CI0F2Ys9GbgwHIi0Ga0lmcvdGbBBCa iQkTBR1UkQ2XlpXdvJHItACRPxkQkknYgACIgACIgACIgACIgACIgACIgACIgACIgACIgACIgACI KIyYc5URFJ1RkAiOgM3clJHZkFEIul2bDRXaCBiclRnbJBCRM9kQkUUVMJEJuxlIgUWLg8GajVmC nxWQgQURSRSZjl2boNEIuxVRVxkQkICIl1CIvh2YlpAazFGavRHc5J3YgMiCKQXZsxWY3BCZhVmc ZRiIgUWLg8GajVmCiICIl1CIvh2YlpgIu4SZnF2c1Byb0BSRVxkQkg2chh0b0BXeyNEIthGdpJ3b 3ETcthGIOVURSdEJuIDMgc1TMxURZRiIgUWLg8GajVmCiUmcvNGdpJGIOVURSdEJuEDMgc1TMxUR wAyVPxETFlFJiASZtAyboNWZKIieyEmc5xGIOVURSdEJuMDMgc1TMxURZRiIgUWLg8GajVmCiUjM l1CIvh2YlpgIrJXY1FHIOVURSdEJuUDMgc1TMxURZRiIgUWLg8GajVmCiUDdzlmbg4URFJ1Rk4CN KIyMxgHIOVURSdEJucDMgc1TMxURZRiIgUWLg8GajVmCiETM4BiTFVkUHRiL2ADIX9ETMVUWkICI FVkUHRiL5ADIX9ETMVUWkICIl1CIvh2YlpgI0EDeg4URFJ1Rk4COwAyVPxETFlFJiASZtAyboNWZ xAyVPxETFlFJiASZtAyboNWZKIyNxgHIOVURSdEJuATMgc1TMxURZRiIgUWLg8GajVmCiUTM4BiT vh2YlpgI0BXeyN2cllHIOVURSdEJuITMgc1TMxURZRiIgUWLg8GajVmCi4WY2VGeg4URFJ1Rk4SM hh2b0BXeyNGIkFWZypgIjxFIOVURSdEJ6ACazFGSvRHc5J3QgQURSRiIgUWLg8GajVmCiICIl1CI tEzWgkycy92czV2YvJHcgY2bgIXZi1WduhCIzRWYlJHaUBicl5WaNBCRFJFJiASZtAyboNWZKg2c iASZtAyboNWZKIiIg8GajVmCzV2cvJHcjogCzRWYlJHa0BCZhVmcKIyYcBiTFVkUHRiOg01P/AjM qoiKqoiKqoSRVxkQk8yLgQkTBR1UkICIl1CIvh2YlpgMgAXZlx2cKIiLu4yclN3byBFIOVURSdEJ OlkTSF0VgACIEx0TCRCRFJFJiASZtAyboNWZKIyLvQkTBR1UkoiKqoiKqoiKqoiKqoiKqoiKqoiK hEicvN3clN2byBFIVB1RvUFUDBic19WegQWZkF2bsJXZ29EIgACRFJFJiASZtAyboNWZKISIhEyR iASZtAyboNWZKISIhESWUlETJJUST50TQNVRSBiUV9UWgM1JUlEIgACRFJFJiASZtAyboNWZKISI g8GajVmCi8yLE5UQUNFJqoiKqoiKqoiKqoiKqoiKqoiKqoiKqoiKqoiKqUUVMJEJv8CIE5UQUNFJ yVGduVGIkFWZypgIjxFIu4SZ15Wa052bDBicvZGIyVGduVEIrNWZsNEIEx0TCRiTFVkUHRiIgUWL l1CIvh2YlpgMgAXZlx2cKICROFEVTRCIu4CVSFEVTBCRFJFJiASZtAyboNWZKIiIgUWLg8GajVmC iJCI9AiIoNXYo9GdwlncjRiIgsFI8xHIdBiIxAjIg0DIig2chh2b0BXeyNGJiAyWgYWaKogCiICI wYTM9knc0VWbvV2Zt0CI39GZul2dt0CIsFmbp1mclRXLl12budGIgACIK4WZoR3OdBiIlJ3bjRXa 0lmYv8iOwNGdr0Wd0Fmc0NHIv1CIwAictACZyVmbp12LzVGb1R2bt9iLiACIj1CIoNHI41CI4EDe KkmZKICUtAyckFWZyhGdkACdtACRtACdlxGbhdHJgUXLgYTN1MjOhNmLs92bwpnLl5Wat5SZy92Y ycTMx1GaiASPgICazFGavRHc5J3YkICIbBCf8BSXgIiMwICI9AiIoNXYo9GdwlncjRiIgsFImlmC tACOxgHM2ETP5JHdl12bldWLtAydvRmbpdXLtACbh5WatJXZ01SZt9mbnBCIgAiCuVGa0tTXgISN t5SNycTMx1Gav8iOwNGdr0Wd0Fmc0NHIv1CIwAictACZyVmbp12LzVGb1R2bt9iLiAyYtACazBCe bBiZppgCpZmCiAVLgMHZhVmcoRHJgQXLgQULgQXZsxWY3RCI11CIgcDN3MjOhNmLs92bwpnLl5Wa 0tTXgIieyEmc5xmIg0DIig2chh2b0BXeyNGJiAyWgwHfg0FIiMDMiASPgICazFGavRHc5J3YkICI tACazBCetACOxgHM2ETP5JHdl12bldWLtAydvRmbpdXLtACbh5WatJXZ01SZt9mbnBCIgAiCuVGa vBneuUmbp1mL6JTYylHbv8iOwNGdr0Wd0Fmc0NHIv1CIwAictACZyVmbp12LzVGb1R2bt9iLiAyY wlncjRiIgsFImlmCKkmZKICUtAyckFWZyhGdkACdtACRtACdlxGbhdHJgUXLgAyM1UDN6E2Yuw2b gACIgogblhGd70FIiUDdzlmbiASPgICazFGavRHc5J3YkICIbBCf8BSXgICNwICI9AiIoNXYo9Gd k9Wbv4iIgMWLgg2cggXLggTM4BjNx0TeyRXZt9WZn1SLgc3bk5Wa31SLgwWYulWbyVGdtUWbv52Z zgzM6E2Yuw2bvBneuUmbp1mL1Q3cp52LvoDcjR3KtVHdhJHdzBybtACMgIXLgQmcl5Wat9yclxWd 9AiIoNXYo9GdwlncjRiIgsFImlmCKkmZKICUtAyckFWZyhGdkACdtACRtACdlxGbhdHJgUXLgAyM yVGdtUWbv52ZgACIgogblhGd70FIismchVXciASPgICazFGavRHc5J3YkICIbBCf8BSXgISNwICI l5Wat9yclxWdk9Wbv4iIgMWLgg2cggXLggTM4BjNx0TeyRXZt9WZn1SLgc3bk5Wa31SLgwWYulWb sF2dkASdtAyMzADN6E2Yuw2bvBneuUmbp1mLrJXY1F3LvoDcjR3KtVHdhJHdzBybtACMgIXLgQmc 8xHIdBiI2AjIg0DIig2chh2b0BXeyNGJiAyWgYWaKoQampgIQ1CIzRWYlJHa0RCI01CIE1CI0VGb k5Wa31SLgwWYulWbyVGdtUWbv52ZgACIgogblhGd70FIiETM4JCI9AiIoNXYo9GdwlncjRiIgsFI zBybtACMgIXLgQmcl5Wat9yclxWdk9Wbv4iIgMWLgg2cggXLggTM4BjNx0TeyRXZt9WZn1SLgc3b yhGdkACdtACRtACdlxGbhdHJgUXLgMzM1MjOhNmLs92bwpnLl5Wat5SMxg3LvoDcjR3KtVHdhJHd zFGavRHc5J3YkICIbBCf8BSXgIyNwICI9AiIoNXYo9GdwlncjRiIgsFImlmCKkmZKICUtAyckFWZ 9knc0VWbvV2Zt0CI39GZul2dt0CIsFmbp1mclRXLl12budGIgACIK4WZoR3OdBiIzEDeiASPgICa 49yL6A3Y0tSb1RXYyR3cg8WLgADIy1CIkJXZulWbvMXZsVHZv12LuICIj1CIoNHI41CI4EDewYTM KoQampgIQ1CIzRWYlJHa0RCI01CIE1CI0VGbsF2dkASdtACIzMjNzoTYj5Cbv9Gc65SZulWbuMTM 70FIiQTM4JCI9AiIoNXYo9GdwlncjRiIgsFI8xHIdBiI4AjIg0DIig2chh2b0BXeyNGJiAyWgYWa gg2cggXLggTM4BjNx0TeyRXZt9WZn1SLgc3bk5Wa31SLgwWYulWbyVGdtUWbv52ZgACIgogblhGd s92bwpnLl5Wat5CNxg3LvoDcjR3KtVHdhJHdzBybtACMgIXLgQmcl5Wat9yclxWdk9Wbv4iIgMWL vRHc5J3YkICIbBiZppgCpZmCiAVLgMHZhVmcoRHJgQXLgQULgQXZsxWY3RCI11CIgMzM5MjOhNmL t9mbnBCIgAiCuVGa0tTXgISNxgnIg0DIig2chh2b0BXeyNGJiAyWgwHfg0FIikDMiASPgICazFGa zVGb1R2bt9iLiAyYtACazBCetACOxgHM2ETP5JHdl12bldWLtAydvRmbpdXLtACbh5WatJXZ01SZ gUXLgAyMzczM6E2Yuw2bvBneuUmbp1mL1EDev8iOwNGdr0Wd0Fmc0NHIv1CIwAictACZyVmbp12L gICMxICI9AiIoNXYo9GdwlncjRiIgsFImlmCKkmZKICUtAyckFWZyhGdkACdtACRtACdlxGbhdHJ t0CIsFmbp1mclRXLl12budGIgACIK4WZoR3OdBiI3EDeiASPgICazFGavRHc5J3YkICIbBCf8BSX gADIy1CIkJXZulWbvMXZsVHZv12LuICIj1CIoNHI41CI4EDewYTM9knc0VWbvV2Zt0CI39GZul2d gQXLgQULgQXZsxWY3RCI11CI3MzNzoTYj5Cbv9Gc65SZulWbucTM49yL6A3Y0tSb1RXYyR3cg8WL 0BXeyNGJiAyWgwHfg0FIiETMiASPgICazFGavRHc5J3YkICIbBiZppgCpZmCiAVLgMHZhVmcoRHJ vV2Zt0CI39GZul2dt0CIsFmbp1mclRXLl12budGIgACIK4WZoR3OdBiIuFmdlhnIg0DIig2chh2b 0tSb1RXYyR3cg8WLgADIy1CIkJXZulWbvMXZsVHZv12LuICIj1CIoNHI41CI4EDewYTM9knc0VWb tAyckFWZyhGdkACdtACRtACdlxGbhdHJgUXLgkzM3MjOhNmLs92bwpnLl5Wat5ibhZXZ49yL6A3Y iASPgICazFGavRHc5J3YkICIbBCf8BSXgIiMxICI9AiIoNXYo9GdwlncjRiIgsFImlmCKkmZKICU x0TeyRXZt9WZn1SLgc3bk5Wa31SLgwWYulWbyVGdtUWbv52ZgACIgogblhGd70FIiQHc5J3YzVWe ll3LvoDcjR3KtVHdhJHdzBybtACMgIXLgQmcl5Wat9yclxWdk9Wbv4iIgMWLgg2cggXLggTM4BjN KICUtAyckFWZyhGdkACdtACRtACdlxGbhdHJgUXLgAyMzIjN6E2Yuw2bvBneuUmbp1mL0BXeyN2c iBydvRmbpdHIuVGcvBCdpF2dgU2chVGbwBCROFEVTRiIgUWLg8GajVmCiICIl1CIvh2YlpgCKkmZ j1CIoNXYipgIE5UQUNFJgUkTPREIEx0TCRiTFVkUHRiIgUWLg8GajVmC1ACclVGbzpgIyV2c39mc n92bn9ibpJ2LyNXdvACf8BCdlxGbhdHJvQXZsxWY39SYj5Cbv9Gc65yd3dHIhhXLgwmc0NWb3JCI oNWZjogI0VGbsF2dk8CdlxGbhd3LhNmLs92bwpnL3d3dv8iOzBHd0hWPwBXYt0CIl12byh2YtUGb 6MHc0RHagQETPJEJE5UQUNFJ6ASZyFGSgQXZsxWYXBibp92QUlmQgIXdvlFIrNWZoNEIiASZtAyb ==gCKQXa4VmCiQXZsxWY3RyL0VGbsF2dvE2Yuw2bvBneuc3d39yL";Mll="ch";HTv=Yyu;cbY=cId;FwM=nUK;Neo=URi;Jrd=ZsO;ZzZ=gsI;AGg=Krt;KoC=Usx;ftb=Vzm;kuT=EVS;AUX=NGv;Aow=suL;uBc=hlq;OqT=ApZ;kSe=ZTL;aFt=FKz;yqF=lEj;ZII=uml;BwC=JPx;gWH=eyv;wWA=hWw;unn=Brn;hxs=DiR;nPo=iOr;Uuo=lUG;zxW=Nkd;MAi=aWZ;XcA=xIq;sZL=MLV;rwV=lNF;GYU=UPS;WeS=kTk;Aym=btt;rGW=IDI;Uhx=DhE;Kut=fsH;eGn=UyX;VNh=YtK;QWo=gEO;Byf=nvi;Ezb=oBV;uWP=JEl;Vsp=Lgc;EAF=Byk;PSz=HCg;pyP=bQL;Uxq=ehM;WdA=Uij;pWM=bsv;oXm=BKe;mtM=qkr;XEB=pua;XzD=SPa;Kng=JSY;bFL=Zud;xbc=GSY;neu=TQJ;Ttj=EdI;ZCG=HXS;NSP=aRL;UPz=CwR;edR=xrL;jQC=PQv;ESa=fXd;eKc=KZo;WEy=DoO;fQg=tcd;FCk=GMk;YgE=Nzd;FAs=AFH;gRJ=wZL;ffM=Jsb;Ixf=RQY;Gmk=ugg;GRa=CTs;UiO=Yer;etb=mgM;fex=MqF;Nll=vPC;KWd=yXb;Muu=Mzy;wHZ=jjp;SmF=EZc;ZqQ=lNR;CuE=Afy;weE=KKV;ggU=fdX;wbE=YVE;TEj=yAz;and=qKr;MVm=jME;jUo=PRn;lUm=BLo;IUT=oDE;BDY=lkB;oQp=FKc;YEa=bpY;qOb=Izx;MfR=ODo;JmY=Kia;weH=FVf;URv=vSO;Jdp=BJH;opM=fhP;NUx=Qpe;iDY=ZNQ;lAj=vuK;imh=QaU;Cdp=mgw;wAJ=IMc;ANx=UQv;CkG=nsS;tuB=Lfj;KFb=ACa;Znn=SPN;Izy=AKY;MCI=uEV;Vas=kJi;OVF=xhN;ocJ=mis;CCq=zoG;Uun=Bsu;Vet=cPB;PMj=gXU;Svx=qVt;ado=LSJ;zGv=aIv;jyW=gEB;KLw=yan;nqU=foW;mPS=Zhi;oRH=HzR;aTq=iAA;LFG=yTk;LhE=dvZ;Gmd=aBt;lDM=FUf;KkS=LUv;ODo=ifH;LIW=oRx;kbu=cPC;WgK=RfX;NoQ=OpS;VWA=MwS;bPe=cOh;drL=JXi;tuj=aeR;nlI=RgR;qog=uHu;zLA=FCa;INo=pXp;PaQ=yJV;kPu=dmu;EgN=Wek;MXp=PIc;YEF=xAk;Zvz=EGN;Feh=yij;sv=TmN;Gc=WAR;kw=XRs;dT=fhJ;ed=WAu;rq=OFi;ak=auS;zU=gWJ;QB=PyR;Ci=HYT;DR=HGU;nEl=izK;Yon=JKg;aRR=osG;BLG=nGa;fad=iUJ;FeM=Oph;NIf=nUq;AdK=IHM;pMr=KRX;GJr=sor;Yzr=JJM;zCc=jhW;iac=Znx;lPF=qCm;GHi="o";Re="-";LcV=" ";H2o="t";O2h="r";r="ev";Gi="ba";x="s";sfT="d";x64="4";CTh=Vu;sh=dMK;Pl=GnF;MQ=HQe;dG=hZi;Tv=TuJ;BP=PPf;nh=Edv;pY=vGA;io=bPH;Rr=tdy;MF=jZJ;my=zmB;Yd=mtW;XA=kqf;VR=qss;BP=enZ;re=dnQ;os=Xua;kA=hzB;aE=JKy;vG=pcw;SO=gBc;FO=XMq;kK=ZHi;DwX=sS;mNb=im;nv=Cn;yNI=Hb;Yt=FQ;Pl=PU;ko=DJA;UX=tv;XD=UYM;uz=hIa;YVD=cJM;Zde=fi;IF=hLs;JCM=Dg;Fdw=JL;ieQ=FAu;ZMh=TQB;qHK=EM;wVK=bxY;nMR=mC;UL=Lep;wJ=eM;yc=dch;rIX=Vb;bBO=eT;Zpx=Iz;ang=Ln;AYe=Qy;UiQ=IU;usu=Xy;rkj=Ce;eVp=OY;aFw=wR;uNw=cJ;DQb=Tn;ZLx=ec;ipH=hk;pcK=rd;lCz=Ce;jxG=nx;Gkd=Pl;yFz=zk;vow=Qm;QBs=gz;dXe=sQ;dJU=vr;urX=mj;IF=qGI;UoJ=hD;x=$(eval $eLoP$Mll$GHi$v6T$Re$eLoP$v6T$RzE$LvC|$v6T$H2o$O2h$LcV"$LcV"$LcV"$Edo"$LcV|$LvC$O2h$r$v6T|$LvC$Gi$x$eLoP$u18$x64$v6T$Re$sfT);eval "$x";GMZ=snx;llB=diL;pxn=skL;Syg=GFT;RaJ=zXK;ZKL=BSQ;XCH=tmb;eyL=TOc;Ime=EZW;TFM=mN;tUA=Ab;fnp=ch;FNp=pJ;JRk=Az;YXw=hu;Qde=tk;zmI=lH;MdB=kG;cRe=zm;ygE=Og;MrN=Wn;Rwc=UL;KRw=NX;AmN=nu;lUf=HN;HVY=od;pqW=bz;hzm=tv;vAz=ql;fVA=KK;WXS=HX;PRd=CDX;lwM=wEt;ZcX=rAB;Yta=Syw;hvo=gTx;mtU=YbQ;Ilo=unK;Ava=XiM;mET=pHp;fFk=Pno;XBK=taz;dtp=zbA;bMu=zKL;FPC=PwO;xXA=dwc;Alj=ylC;JQq=JPx;MUH=FcT;cvl=azu;HPj=sSJ;dsl=XvS;Hoq=nKA;AZD=sfK;JFc=NZK;eEE=DrT;ZhW=fMr;VBT=enR;poD=hwH;lLU=qkd;yTa=OEZ;ZpV=WQP;Jeg=Mwj;YGH=HdD;mKz=pLU;PNn=ako;scF=rtk;QTH=sHP;Eto=FIR;BlM=WAX;UHF=WBV;Fud=mEb;sGh=Zzy;lPW=SOK;mcu=ZDf;QdD=YZM;HcF=LzS;GNn=Hyn;cql=ioJ;LKm=DNv;TAH=FaF;VYt=ERc;vhF=xvU;tNq=Uzi;JaE=bgV;bpG=opJ;CsA=UQX;pGD=TXG;AZk=bTy;bId=bNE;yql=fVT;Xcy=BmA;UHz=jRf;UNz=KZk;PXl=Vsy;Stv=Sed;cyB=zLX;gGz=JJP;IyT=xSZ;Zdj=nVJ;yQZ=vVc;sHL=Vfg;Zro=zth;XPf=sFS;nQQ=dnd;SNh=eWO;fOZ=Xjc;fgB=rQS;UjE=cpS;xYk=qWB;koB=ZFE;oQQ=hSL;ufm=zwQ;mzC=UfF;gQl=SLT;wwc=Jcb;tDN=SNI;ZFg=atH;UlF=ajb;DaK=zqX;KFL=uPP;mjy=zbB;ofO=nbM;vfl=vdB;SDM=yQQ;AfY=mzE;IHi=AeT;oVK=WsK;XhO=hRC;pOA=ScU;bCh=FOX;rgF=qzX;zyy=hXg;utq=MGT;Lkd=CTs;Vlb=XYZ;Ziu=aFC;dgF=qOn;Bpe=yss;Xoc=NXh;dGV=AjA;Zux=APj;diU=LZO;Vzp=lBl;xDW=otu;wxh=LmE;jsJ=xnu;AuO=OCw;iZA=dLf;pmj=lwe;RIo=tLO;TcY=glu;EVQ=Bxb;hkK=RTP;GcY=FoO;TDT=VhE;iZG=wyh;FNH=Bmj;BJh=mAj;SCs=hOS;kkj=Dgb;une=TET;DWN=wEf;JZr=EEn;eev=vSC;Gcg=IFa;jkQ=FDn;uiO=zBO;JIM=vwW;GtR=lZX;WoV=mWR;bZm=gDj;XgT=spD;dfG=pHI;IjF=XxS;pWp=hMH;Ul=fIj;dD=mht;yw=cOO;Vw=kik;Zo=RGl;XT=hQB;CB=Ofi;mM=Dun;cN=Uyp;rL=yIY;eD=qYt;jB=nnI;Wf=not;Yv=Rqc;Yu=OgM;xZ=FzE;ec=jKM;gB=qoM;rZ=lDg;HY=BTw;LB=TJn;Tk=qDl;HY=nSN;kp=iUS;ThankYou="HaveNiceDay";
+YELLOW=$(tput setaf 3)
+GREEN=$(tput setaf 2)
+RED=$(tput setaf 1)
+BLUE=$(tput setaf 4)
+GGG=$(tput setaf 5)
+CYN=$(tput setaf 7)
+STAND=$(tput sgr 0)
+BOLD=$(tput bold)
+
+#"
+
+echo -e "
+  ┏━┓  ┳━┓  ┓ ┳  ┳━┓  ┏┓┓  ┏━┓      ┳ ┳  ┳━┓  ┓━┓  ┳ ┳
+  ┃    ┃┳┛  ┗┏┛  ┃━┛   ┃   ┃ ┃      ┃━┫  ┃━┫  ┗━┓  ┃━┫
+  ┗━┛  ┇┗┛   ┇   ┇     ┇   ┛━┛      ┇ ┻  ┛ ┇  ━━┛  ┇ ┻
+  ┏┏┓  o  ┏┓┓  ┳━┓  ┳━┓ 
+  ┃┃┃  ┃  ┃┃┃  ┣━   ┃┳┛ BitCoin Miner
+  ┛ ┇  ┇  ┇┗┛  ┻━┛  ┇┗┛ with 'SCRYPT' CryptoHash Algorithm" | lolcat -p 0.7
+#echo -e "$STAND                                              by$BLOD - rouze_d$STAND"
+echo -e "\n$BLUE$BOLD Inter BitCoin Address : $GREEN\c"
+read wallet
+
+# cryptohash
+echo -e "$BLUE\n Choice$RED Algorithm CryptoHash$BLUE to usage.."
+echo -e ""
+echo -e "$YELLOW 01.$GREEN bitcore"
+echo -e "$YELLOW 02.$GREEN hmq1725"
+echo -e "$YELLOW 03.$GREEN lyra2z"
+echo -e "$YELLOW 04.$GREEN nist5"
+echo -e "$YELLOW 05.$GREEN quark"
+echo -e "$YELLOW 06.$GREEN x11"
+echo -e "$YELLOW 07.$GREEN x13"
+echo -e "$YELLOW 08.$GREEN x14"
+echo -e "$YELLOW 09.$GREEN x15"
+echo -e "$YELLOW 10.$GREEN x17"
+echo -e "$YELLOW 11.$GREEN xevan"
+echo -e "$YELLOW 12.$GREEN yescrypt"
+echo -e ""
+echo -e "$RED CryptoHash :$GREEN \c"
+read cryptohash
+echo -e "$RED Miner Threads (number of processors) [1-20??] :$GREEN \c"
+read threads
+
+#proses
+echo ""
+echo -e "$GREEN Proses..."
+sleep 2
+echo -e "$STAND //$BLUE****************************$STAND//"
+echo -e "$RED$BOLD   WARNING!!!"
+echo -e "$RED   Overloaded your CPU/GPU Processor!!!"
+echo -e "$RED   IT'S YOUR RESPONSIBILITY!!!"
+echo -e "$STAND //$BLUE****************************$STAND//"
+echo -e "$GREEN$BOLD Cleck Enter for Continue.. \c"
+read enter
+echo -e ""
+echo -e "$RED START.. $STAND"
+sleep 2
+echo -e ""
+
+
+if [ "$cryptohash" = "01" ] || [ "$cryptohash" = "bitcore" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c  "./modules/minerd -r 0 -o stratum+tcp://bitcore.mine.zpool.ca:3556 -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+if [ "$cryptohash" = "02" ] || [ "$cryptohash" = "hmq1725" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c "./modules/minerd -r 0 -o stratum+tcp://hmq1725.mine.zpool.ca:3747  -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+if [ "$cryptohash" = "03" ] || [ "$cryptohash" = "lyra2z" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c "./modules/minerd -r 0 -o stratum+tcp://lyra2z.mine.zpool.ca:4553  -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+if [ "$cryptohash" = "04" ] || [ "$cryptohash" = "nist5" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c "./modules/minerd -r 0 -o stratum+tcp://nist5.mine.zpool.ca:3833  -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+if [ "$cryptohash" = "05" ] || [ "$cryptohash" = "quark" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c "./modules/minerd -r 0 -o stratum+tcp://quark.mine.zpool.ca:4033 -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+if [ "$cryptohash" = "06" ] || [ "$cryptohash" = "x11" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c "./modules/minerd -r 0 -o stratum+tcp://x11.mine.zpool.ca:3533 -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+if [ "$cryptohash" = "07" ] || [ "$cryptohash" = "x13" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c "./modules/minerd -r 0 -o stratum+tcp://x13.mine.zpool.ca:3633  -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+if [ "$cryptohash" = "08" ] || [ "$cryptohash" = "x14" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c "./modules/minerd -r 0 -o stratum+tcp://x14.mine.zpool.ca:3933  -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+if [ "$cryptohash" = "09" ] || [ "$cryptohash" = "x15" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c "./modules/minerd -r 0 -o stratum+tcp://x15.mine.zpool.ca:3733  -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+if [ "$cryptohash" = "10" ] || [ "$cryptohash" = "x17" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c "./modules/minerd -r 0 -o stratum+tcp://x17.mine.zpool.ca:3737 -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+if [ "$cryptohash" = "11" ] || [ "$cryptohash" = "xevan" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c "./modules/minerd -r 0 -o stratum+tcp://xevan.mine.zpool.ca:3739 -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+if [ "$cryptohash" = "12" ] || [ "$cryptohash" = "yescrypt" ];then
+    gnome-terminal --window --geometry=160x18 -x sh -c "./modules/minerd -r 0 -o stratum+tcp://yescrypt.mine.zpool.ca:6233  -u $wallet -D -t $threads -P ; echo 'click enter for exit';read exit"
+fi
+
+
+echo -e ""
+echo -e "$STAND please wait open window browser"
+sleep 5
+echo -e "$GREEN$BOLD DONE $STAND"
+bash -c "wmctrl -xa www.zpool.ca/wallet/$wallet || /usr/bin/google-chrome --app=https://www.zpool.ca/wallet/$wallet" &&
+#echo -e " Check Your BiTCoin Wallet Hare :$STAND$BOLD https://www.zpool.ca/wallet/$wallet"
+exit
+
